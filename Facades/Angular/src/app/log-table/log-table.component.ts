715538e09a9982dbc4f075124fc291dbfdc2db97
Martin Lukomski <mlm@NBDR248.salt-solutions.de>
2020-03-06 12:44:23 +0100

    REFACTOR structure changed

--- Facades/Angular/src/app/log-table/log-table.component.ts
+++ Facades/Angular/src/app/log-table/log-table.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { IColumnDef, FilterChip } from '../data-table/data-table.component';
-import datatableJson from '../../assets/json/datatable.json';
 import { IWidgetDataTable } from '../widgets/interfaces/data-table.interface';
 
-
+const ACTION_SHOW_WIDGET = 'exface.Core.ShowWidget';
+const URL_FACADE    = 'http://localhost/exface/exface/api/angular';
+const URL_RESOURCE  = 'angular-test-2';
 
 export interface DataRow {}
 
@@ -19,21 +19,16 @@ export interface DataResponse {
   success?: string;
 }
 
-const ACTION_SHOW_WIDGET = 'exface.Core.ShowWidget';
-const URL_FACADE    = 'http://localhost/exface/exface/api/angular';
-const URL_RESOURCE  = 'angular-test';
-
 @Component({
   selector: 'app-log-table',
   templateUrl: './log-table.component.html',
   styleUrls: ['./log-table.component.css']
 })
 export class LogTableComponent implements OnInit {
-  response: DataResponse = {rows: []};
 
   structure: IWidgetDataTable;
 
-  rows: DataRow[];
+  resource: string = URL_RESOURCE;
 
   constructor(private http: HttpClient) { }
 
@@ -41,49 +36,10 @@ export class LogTableComponent implements OnInit {
    
     // Load JSON description of widget
     // When loaded, save it and load data of table
-    this.http.get<IWidgetDataTable>(URL_FACADE + '?action=' + ACTION_SHOW_WIDGET + '&resource=' + URL_RESOURCE).subscribe(
+    this.http.get<IWidgetDataTable>(URL_FACADE + '?action=' + ACTION_SHOW_WIDGET + '&resource=' + this.resource).subscribe(
       (data: IWidgetDataTable) => {
         this.structure = data;
-        this.loadData();
-      }
-    );
-
-  }
-
-  loadData(chips?: FilterChip[]){
-    const params = {
-      action: this.structure.lazy_loading_action.alias,
-      resource: URL_RESOURCE,
-      element: this.structure.id,
-      object: this.structure.object_alias,
-      q: '',
-      'data[oId]': this.structure.lazy_loading_action.object_alias,
-      sort: 'CREATED_ON',
-      order: 'desc',
-      start: '0',      
-      length: '150',
-    };
-
-    if (chips && chips.length > 0) {
-      params['data[filters][operator]'] = 'AND';
-
-      chips.forEach((chip: FilterChip, index: number) => {
-        params['data[filters][conditions][' + index + '][expression]'] = chip.property;
-        params['data[filters][conditions][' + index + '][value]'] = chip.value;
-        params['data[filters][conditions][' + index + '][comperator]'] = '==';
-        params['data[filters][conditions][' + index + '][object_alias]'] = 'exface.Core.MESSAGE';
-
-      });
-    }
-    this.http.get<DataResponse>(URL_FACADE,{params}).subscribe(
-      (response: DataResponse) => {
-        this.response = response;
       }
     );
   }
-
-  onFilterChange(chips:FilterChip[]){
-    this.loadData(chips);    
-  }
-
 }
