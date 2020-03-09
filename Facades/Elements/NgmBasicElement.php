7edd65015e41226e8f4342c6c2b9c3e27323206c
Andrej Kabachnik <andrej_ka@gmx.de>
2020-03-09 13:09:44 +0100

    REFACTOR improved automatic JSON config generation in NgmBasicElement

--- Facades/Elements/NgmBasicElement.php
+++ Facades/Elements/NgmBasicElement.php
@@ -9,6 +9,7 @@ use exface\Core\Interfaces\iCanBeConvertedToUxon;
 use exface\Core\DataTypes\StringDataType;
 use exface\Core\CommonLogic\UxonObject;
 use exface\Core\Interfaces\Actions\ActionInterface;
+use exface\Core\Exceptions\RuntimeException;
 
 /**
  *
@@ -24,6 +25,11 @@ class NgmBasicElement extends AbstractJqueryElement
         return $this->buildJsonFromWidget($this->getWidget());
     }
     
+    /**
+     * 
+     * @param WidgetInterface $widget
+     * @return array
+     */
     protected function buildJsonFromWidget(WidgetInterface $widget) : array
     {
         $json = $this->buildJsonFromObject($widget);
@@ -31,6 +37,11 @@ class NgmBasicElement extends AbstractJqueryElement
         return $json;
     }
     
+    /**
+     * 
+     * @param ActionInterface $action
+     * @return array
+     */
     protected function buildJsonFromAction(ActionInterface $action) : array
     {
         return [
@@ -39,6 +50,12 @@ class NgmBasicElement extends AbstractJqueryElement
         ];
     }
     
+    /**
+     * Generic config JSON generator for anything, that has a UXON model.
+     * 
+     * @param iCanBeConvertedToUxon $object
+     * @return array
+     */
     protected function buildJsonFromObject(iCanBeConvertedToUxon $object) : array
     {
         $json = [];
@@ -50,25 +67,51 @@ class NgmBasicElement extends AbstractJqueryElement
             if (in_array($property, $excludedProps)) {
                 continue;
             }
-            $getterMethod = 'get' . StringDataType::convertCaseUnderscoreToPascal($property);
-            if (method_exists($object, $getterMethod)) {
-                try {
-                    $val = call_user_func([$object, $getterMethod]);
-                } catch (\Throwable $e) {
-                    // TODO
-                    continue;
-                }
-                
-                if (($value = $this->buildJsonValue($val, $property)) !== null) {
-                    $json[$property] = $value;
-                }
-                
+            if (($value = $this->buildJsonPropertyValue($object, $property)) !== null) {
+                $json[$property] = $value;
             }
         }
         return $json;
     }
     
-    protected function buildJsonValue($value, string $property)
+    /**
+     * 
+     * @param iCanBeConvertedToUxon $object
+     * @param string $property
+     * @return NULL|array|string
+     */
+    protected function buildJsonPropertyValue(iCanBeConvertedToUxon $object, string $property)
+    {
+        try {
+            return $this->buildJsonPropertyValueViaGetter($object, $property);
+        } catch (\Throwable $e) {
+            // TODO
+            return null;
+        }
+    }
+    
+    /**
+     *
+     * @param iCanBeConvertedToUxon $object
+     * @param string $property
+     * @return NULL|array|string
+     */
+    protected function buildJsonPropertyValueViaGetter(iCanBeConvertedToUxon $object, string $property)
+    {
+        $getterMethod = 'get' . StringDataType::convertCaseUnderscoreToPascal($property);
+        if (method_exists($object, $getterMethod)) {
+            $val = call_user_func([$object, $getterMethod]);
+            return $this->buildJsonValue($val);
+        }
+        throw new RuntimeException('Cannot get UXON property "' . $property . '" of class "' . get_class($object) . '" via getter-method: method "' . $getterMethod . '()" not found!');
+    }
+    
+    /**
+     * 
+     * @param string|array|object $value
+     * @return NULL|array|string
+     */
+    protected function buildJsonValue($value)
     {
         switch (true) {
             case is_scalar($value):
@@ -78,11 +121,11 @@ class NgmBasicElement extends AbstractJqueryElement
             case is_array($value):
                 $result = [];
                 foreach ($value as $key => $val) {
-                    $result[$key] = $this->buildJsonValue($val, $property);
+                    $result[$key] = $this->buildJsonValue($val);
                 }
                 return $result;
             case $value instanceof WidgetInterface:
-                return $this->buildJsonFromWidget($value);
+                return $this->getFacade()->getElement($value)->buildJson();
             case $value instanceof ActionInterface:
                 return $this->buildJsonFromAction($value);
         }
