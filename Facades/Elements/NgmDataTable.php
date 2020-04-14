590d3e126d22dd83c11c8667c44a10a546a4a46e
mlm <mlm@NBDR248.salt-solutions.de>
2020-04-14 14:46:33 +0200

    NEW ngmDataTable

---
+++ Facades/Elements/NgmDataTable.php
@@ -0,0 +1,29 @@
+<?php
+namespace axenox\AngularMaterialFacade\Facades\Elements;
+
+use exface\Core\Interfaces\iCanBeConvertedToUxon;
+
+/**
+ *
+ * @method AngularMaterialFacade getFacade()
+ *        
+ * @author Andrej Kabachnik
+ *        
+ */
+class NgmDataTable extends NgmBasicElement
+{
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \axenox\AngularMaterialFacade\Facades\Elements\NgmBasicElement::buildJsonPropertyValue()
+     */
+    protected function buildJsonPropertyValue(iCanBeConvertedToUxon $object, $property)
+    {
+        switch ($property) {
+            case 'caption':
+                return $this->getWidget()->getCaption() ? $this->getWidget()->getCaption() : $this->getMetaObject()->getName();
+        }
+        return parent::buildJsonPropertyValue($object, $property);
+    }
+}
