ce8014f236c44e117e3a1953359b4f001b3b570c
Lukomski <mlm@salt-solutions.de>
2020-06-04 15:26:12 +0200

    NEW improved menu-tree

--- Facades/Angular/src/app/app-routing.module.ts
+++ Facades/Angular/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DataTableComponent } from './widgets/data-table/data-table.component';
 import { PageComponent } from './page/page.component';
+import { ExternalPageComponent } from './external-page/external-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/page/angular-test', pathMatch: 'full'},
   {path: 'table', component: DataTableComponent},
   {path: 'page/:pageSelector', component: PageComponent},
+  {path: 'ext-page/:url', component: ExternalPageComponent},
 ];
 
 @NgModule({
