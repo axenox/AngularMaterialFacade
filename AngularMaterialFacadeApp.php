c356d2fe9562b9086d603b97cf7646a838d9f98f
Andrej Kabachnik <andrej_ka@gmx.de>
2020-06-09 14:44:05 +0200

    NEW AngularInstaller

--- AngularMaterialFacadeApp.php
+++ AngularMaterialFacadeApp.php
@@ -6,6 +6,10 @@ use exface\Core\Facades\AbstractHttpFacade\HttpFacadeInstaller;
 use exface\Core\CommonLogic\Model\App;
 use exface\Core\Factories\FacadeFactory;
 use exface\ModxCmsConnector\CommonLogic\Installers\ModxCmsTemplateInstaller;
+use exface\Core\Interfaces\ConfigurationInterface;
+use axenox\AngularMaterialFacade\Facades\AngularMaterialFacade;
+use exface\Core\CommonLogic\Filemanager;
+use exface\Core\DataTypes\FilePathDataType;
 
 class AngularMaterialFacadeApp extends App
 {
@@ -24,8 +28,13 @@ class AngularMaterialFacadeApp extends App
         
         // Install routes
         $tplInstaller = new HttpFacadeInstaller($this->getSelector());
-        $tplInstaller->setFacade(FacadeFactory::createFromString('axenox.AngularMaterialFacade.AngularMaterialFacade', $this->getWorkbench()));
+        $facade = FacadeFactory::createFromString('axenox.AngularMaterialFacade.AngularMaterialFacade', $this->getWorkbench());
+        $tplInstaller->setFacade($facade);
         $installer->addInstaller($tplInstaller);
+        
+        $angularInstaller = new AngularInstaller($this->getSelector());
+        $angularInstaller->setFacade($facade);
+        $installer->addInstaller($angularInstaller);
         
         return $installer;
     }
