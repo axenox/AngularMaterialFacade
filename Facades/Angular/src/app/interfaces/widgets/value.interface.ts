7583777ffeea96f19dcd497abc7b2609e1fb940c
Andrej Kabachnik <andrej_ka@gmx.de>
2020-06-18 12:15:26 +0200

    NEW value property in value widgets

--- Facades/Angular/src/app/interfaces/widgets/value.interface.ts
+++ Facades/Angular/src/app/interfaces/widgets/value.interface.ts
@@ -3,4 +3,5 @@ import { IWidgetInterface } from './widget.interface';
 export interface IWidgetValueInterface extends IWidgetInterface {
     attribute_alias: string;
     data_column_name: string;
+    value: string;
 }
